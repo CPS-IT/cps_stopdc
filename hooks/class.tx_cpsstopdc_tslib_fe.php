e8381800f44a0bf7417de3a8b876337789546fef
Nicole Cordes <cordes@cps-it.de>
2011-09-14 00:36:35 +0200

    Added new function to support canonical urls

--- hooks/class.tx_cpsstopdc_tslib_fe.php
+++ hooks/class.tx_cpsstopdc_tslib_fe.php
@@ -25,18 +25,19 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
+require_once(PATH_tslib.'class.tslib_content.php');
+
 class tx_cpsstopdc {
 
 	/**
 	*	Checks current url with those provided by typolink function (latest one)
 	*
-	*	@param	string				$theTable: Database table
-	*	@param	string				$parentField: Database field to check with third parameter
-	*	@param	mixed					$uids: Uids of items
-	*	@return	string				An rootline array
+	*	@param	array					$params: Parameter given from caller
+	*	@param	array					$pObj: Parent object (tslib_fe)
+	*	@return	void
 	*
 	*/
-	function isOutputting($params, &$pObj) {
+	function checkDataSubmission(&$pObj) {
 
 		$this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cps_stopdc']);
 
@@ -47,10 +48,7 @@ class tx_cpsstopdc {
 
 			// Prepare array with query string information for typolink function
 			// Strip id from array as it's an own parameter
-			$queryArray = tx_cpsdevlib_div::queryStringToArray(t3lib_div::getIndpEnv('QUERY_STRING'));
-			if (isset($queryArray['id'])) {
-				unset($queryArray['id']);
-			}
+			$queryArray = tx_cpsdevlib_div::queryStringToArray(t3lib_div::getIndpEnv('QUERY_STRING'), 'id');
 
 			// Prepare urls for comparison
 			$currentUrlArray = parse_url(t3lib_div::getIndpEnv('REQUEST_URI'));
@@ -85,6 +83,61 @@ class tx_cpsstopdc {
 			if (t3lib_extMgm::isLoaded('cooluri')) {
 				$GLOBALS['TYPO3_DB']->debugOutput = 1;
 				$GLOBALS['TYPO3_DB']->exec_UPDATEquery('link_oldlinks', 'DATEDIFF(NOW(), tstamp) >= 0', array('tstamp' => 'DATE_ADD(tstamp, INTERVAL ' . $this->extConf['extendExpiration'] . ' DAY)'), 'tstamp');
+			}
+		}
+	}
+
+
+
+	/**
+	*	Add canonical url if not already included
+	*
+	*	@param	array					$params: Parameter given from caller
+	*	@param	array					$pObj: Parent object (tslib_fe)
+	*	@return	void
+	*
+	*/
+	function contentPostProc_output(&$params, &$pObj) {
+
+		$this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cps_stopdc']);
+
+		// Check only if it's enabled in extension manager
+		if ($this->extConf['canonicalUrl']) {
+			// Get header suppart
+			$headerStart = strpos($pObj->content, ($pObj->pSetup['headTag'] ? $pObj->pSetup['headTag'] : '<head>'));
+			$headerEnd = strpos($pObj->content, '</head>') + 7;
+			$headerData = substr($pObj->content, $headerStart, $headerEnd - $headerStart);
+
+			// Only add canonical url when not already exists
+			if (strpos($headerData, 'rel="canonical"') === false) {
+
+				if ($this->extConf['removeVarsInCanonicalUrl'] == 'all') {
+					$queryArray = array();
+				} else {
+					$queryArray = tx_cpsdevlib_div::queryStringToArray(t3lib_div::getIndpEnv('QUERY_STRING'), $this->extConf['removeVarsInCanonicalUrl']);
+				}
+
+				// Store mount point in temp variable
+				$tempMP = $pObj->MP;
+				$pObj->MP = '';
+
+				// Store linkVars in temp variable
+				$tempLinkVars = $pObj->linkVars;
+				$pObj->linkVars = '';
+
+				// Get id related to content page (to support content_from_pid)
+				$id = $pObj->contentPid;
+
+				// Get url and link tag
+				$url = $pObj->cObj->getTypoLink_URL($id, $queryArray);
+				$canonical = '<link rel="canonical" href="' . (($pObj->config['config']['baseURL']) ? $pObj->config['config']['baseURL'] : '') . $url . '" ' . tx_cpsdevlib_extmgm::getEndingSlash() . '>';
+
+				// Restore mount point and link vars
+				$pObj->MP = $tempMP;
+				$pObj->linkVars = $tempLinkVars;
+
+				// Replace </head> tag with canonical url
+				$pObj->content = str_replace('</head>', $canonical . LF . '</head>', $pObj->content);
 			}
 		}
 	}
