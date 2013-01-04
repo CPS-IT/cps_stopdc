39f457f802bb87f490bdd017c15e8607bb045602
Nicole Cordes <cordes@cps-it.de>
2013-01-04 09:26:04 +0100

    Fixed code according to CGL

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.2.0-0.0.0',
+			'typo3' => '4.5.0-0.0.0',
 			'cps_devlib' => '0.6.0-0.0.0',
 		),
 		'conflicts' => array(
--- hooks/class.tx_cpsstopdc_tslib_fe.php
+++ hooks/class.tx_cpsstopdc_tslib_fe.php
@@ -25,83 +25,112 @@
 *  This copyright notice MUST APPEAR in all copies of the script!
 ***************************************************************/
 
-require_once(PATH_tslib.'class.tslib_content.php');
-
 class tx_cpsstopdc {
 
 	/**
-	*	Checks current url with those provided by typolink function (latest one)
+	 * @var array
+	 */
+	var $extConf = array();
+
+	/**
+	* Checks current url with those provided by typolink function (latest one)
 	*
-	*	@param	array					$params: Parameter given from caller
-	*	@param	array					$pObj: Parent object (tslib_fe)
-	*	@return	void
-	*
+	* @param tslib_fe $pObj Parent object
+	 *
+	* @return void
 	*/
-	function checkDataSubmission(&$pObj) {
+	public function checkDataSubmission(&$pObj) {
 
 		$this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cps_stopdc']);
 
-		// If any id was found so far
+			// If any id was found so far
 		if (intval($GLOBALS['TSFE']->id) > 0) {
 
 			$local_cObj = t3lib_div::makeInstance('tslib_cObj');
 
-			// Prepare array with query string information for typolink function
-			// Strip id from array as it's an own parameter
-			// Decode url as it's encoded by TYPO3 function again
+				// Prepare array with query string information for typolink function
+				// Strip id from array as it's an own parameter
+				// Decode url as it's encoded by TYPO3 function again
 			$queryArray = tx_cpsdevlib_div::queryStringToArray(t3lib_div::getIndpEnv('QUERY_STRING'), 'id');
 			foreach ($queryArray as $key => $value) {
 				$queryArray[$key] = rawurldecode($value);
 			}
 			unset($key, $value);
 
-			// Prepare urls for comparison
+				// Prepare urls for comparison
 			$currentUrlArray = parse_url(t3lib_div::getIndpEnv('REQUEST_URI'));
 			if ($currentUrlArray['path'][0] != '/') $currentUrlArray['path'] = '/' . $currentUrlArray['path'];
-			// Decode query part for comparison reason
+				// Decode query part for comparison reason
 			if (isset($currentUrlArray['query'])) $currentUrlArray['query'] = rawurldecode($currentUrlArray['query']);
 			$latestUrl = $local_cObj->getTypoLink_URL($GLOBALS['TSFE']->id, $queryArray);
 			$latestUrlArray = parse_url($latestUrl);
 			if ($latestUrlArray['path'][0] != '/') $latestUrlArray['path'] = '/' . $latestUrlArray['path'];
-			// Decode query part for comparison reason
+				// Decode query part for comparison reason
 			if (isset($latestUrlArray['query'])) $latestUrlArray['query'] = rawurldecode($latestUrlArray['query']);
 
-			// Check for site root
+				// Check for site root
 			if (($GLOBALS['TSFE']->page['is_siteroot']) AND (!count($queryArray))) {
 				$latestUrlArray['path'] = '/';
 				$latestUrl = '/';
 			}
 
-			// Compare arrays
+				// Compare arrays
 			$resultArray = array_diff_assoc($currentUrlArray, $latestUrlArray);
 
-			// Redirect if there are any differences
+				// Redirect if there are any differences
 			if (count($resultArray)) {
 
-				// Send header from extension configuration
+					// Send header from extension configuration
 				if ($this->extConf['header']) {
 					header($this->extConf['header']);
 				}
 
-				// Redirect
+					// Redirect
 				header('Location: ' . t3lib_div::locationHeaderUrl($latestUrl));
 				exit;
 			}
 		}
 
-		// Extend realurl expiration dates
-		if ($this->extConf['useRealurl'] == 1) {
-			if (t3lib_extMgm::isLoaded('realurl')) {
-				$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_realurl_pathcache', 'expire <= ' . time() . ' AND expire > 0', array('expire' => 'expire + ' . ($this->extConf['extendExpiration'] * 24 * 60 * 60)), 'expire');
-				$GLOBALS['TYPO3_DB']->exec_UPDATEquery('tx_realurl_uniqalias', 'expire <= ' . time() . ' AND expire > 0', array('expire' => 'expire + ' . ($this->extConf['extendExpiration'] * 24 * 60 * 60)), 'expire');
-			}
-		}
-
-		// Extend CoolURI expiration dates
+			// Extend CoolURI expiration dates
 		if ($this->extConf['useCoolUri']) {
 			if (t3lib_extMgm::isLoaded('cooluri')) {
 				$GLOBALS['TYPO3_DB']->debugOutput = 1;
-				$GLOBALS['TYPO3_DB']->exec_UPDATEquery('link_oldlinks', 'DATEDIFF(NOW(), tstamp) >= 0', array('tstamp' => 'DATE_ADD(tstamp, INTERVAL ' . $this->extConf['extendExpiration'] . ' DAY)'), 'tstamp');
+				$GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+					'link_oldlinks',
+					'DATEDIFF(NOW(), tstamp) >= 0',
+					array(
+						'tstamp' => 'DATE_ADD(tstamp, INTERVAL ' . $this->extConf['extendExpiration'] . ' DAY)'
+					),
+					array(
+						0 => 'tstamp'
+					)
+				);
+			}
+		}
+
+			// Extend realurl expiration dates
+		if ($this->extConf['useRealurl'] == 1) {
+			if (t3lib_extMgm::isLoaded('realurl')) {
+				$GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+					'tx_realurl_pathcache',
+					'expire <= ' . time() . ' AND expire > 0',
+					array(
+						'expire' => 'expire + ' . ($this->extConf['extendExpiration'] * 24 * 60 * 60)
+					),
+					array(
+						0 => 'expire'
+					)
+				);
+				$GLOBALS['TYPO3_DB']->exec_UPDATEquery(
+					'tx_realurl_uniqalias',
+					'expire <= ' . time() . ' AND expire > 0',
+					array(
+						'expire' => 'expire + ' . ($this->extConf['extendExpiration'] * 24 * 60 * 60)
+					),
+					array(
+						0 => 'expire'
+					)
+				);
 			}
 		}
 	}
@@ -109,28 +138,28 @@ class tx_cpsstopdc {
 
 
 	/**
-	*	Add canonical url if not already included
+	* Add canonical url if not already included
 	*
-	*	@param	array					$params: Parameter given from caller
-	*	@param	array					$pObj: Parent object (tslib_fe)
-	*	@return	void
-	*
+	* @param array $params Parameter given from caller
+	* @param tslib_fe $pObj Parent object
+	 *
+	* @return void
 	*/
-	function contentPostProc_output(&$params, &$pObj) {
+	public function contentPostProc_output(&$params, &$pObj) {
 
 		$this->extConf = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['cps_stopdc']);
 
-		// Check only if it's enabled in extension manager
+			// Check only if it's enabled in extension manager
 		if ($this->extConf['canonicalUrl']) {
-			// Get header suppart
+				// Get header suppart
 			$headerStart = strpos($pObj->content, ($pObj->pSetup['headTag'] ? $pObj->pSetup['headTag'] : '<head>'));
 			$headerEnd = strpos($pObj->content, '</head>') + 7;
 			$headerData = substr($pObj->content, $headerStart, $headerEnd - $headerStart);
 
-			// Only add canonical url when not already exists
-			if (strpos($headerData, 'rel="canonical"') === false) {
+				// Only add canonical url when not already exists
+			if (strpos($headerData, 'rel="canonical"') === FALSE) {
 
-				// Use local cObj as cached pages haven't any cObj
+					// Use local cObj as cached pages haven't any cObj
 				$local_cObj = t3lib_div::makeInstance('tslib_cObj');
 
 				if ($this->extConf['removeVarsInCanonicalUrl'] == 'all') {
@@ -139,33 +168,35 @@ class tx_cpsstopdc {
 					$queryArray = tx_cpsdevlib_div::queryStringToArray(t3lib_div::getIndpEnv('QUERY_STRING'), $this->extConf['removeVarsInCanonicalUrl']);
 				}
 
-				// Store mount point in temp variable
+					// Store mount point in temp variable
 				$tempMP = $pObj->MP;
 				$pObj->MP = '';
 
-				// Store linkVars in temp variable
+					// Store linkVars in temp variable
 				$tempLinkVars = $pObj->linkVars;
 				$pObj->linkVars = '';
 
-				// Get id related to content page (to support content_from_pid)
+					// Get id related to content page (to support content_from_pid)
 				$id = $pObj->contentPid;
 
-				// Get url and link tag
+					// Get url and link tag
 				$url = htmlspecialchars($local_cObj->getTypoLink_URL($id, $queryArray));
-				$canonical = '<link rel="canonical" href="' . (($pObj->config['config']['baseURL']) ? $pObj->config['config']['baseURL'] : '') . $url . '" ' . tx_cpsdevlib_extmgm::getEndingSlash() . '>';
+				$canonical = '<link rel="canonical" ' .
+					'href="' . (($pObj->config['config']['baseURL']) ? $pObj->config['config']['baseURL'] : '') . $url . '" ' .
+					tx_cpsdevlib_extmgm::getEndingSlash() . '>';
 
-				// Restore mount point and link vars
+					// Restore mount point and link vars
 				$pObj->MP = $tempMP;
 				$pObj->linkVars = $tempLinkVars;
 
-				// Replace </head> tag with canonical url
+					// Replace </head> tag with canonical url
 				$pObj->content = str_replace('</head>', $canonical . LF . '</head>', $pObj->content);
 			}
 		}
 	}
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cps_stopdc/class.tx_cpsstopdc.php'])    {
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/cps_stopdc/class.tx_cpsstopdc.php']);
+if (defined('TYPO3_MODE') && $GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/cps_stopdc/class.tx_cpsstopdc.php'])    {
+	include_once($GLOBALS['TYPO3_CONF_VARS'][TYPO3_MODE]['XCLASS']['ext/cps_stopdc/class.tx_cpsstopdc.php']);
 }
 ?>
