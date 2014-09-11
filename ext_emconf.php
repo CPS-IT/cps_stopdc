27b0aa873dfcc9eb02bfd2b8a1ce5c7015d7c84a
Nicole Cordes <cordes@cps-it.de>
2014-09-11 19:24:53 +0200

    [RELEASE] Release of cps_stopdc 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "cps_stopdc".
  *
- * Auto generated 13-03-2013 01:55
+ * Auto generated 11-09-2014 19:23
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Forces TYPO3 to use the latest url for any page. This helps to avoid duplicate content.',
 	'category' => 'fe',
 	'shy' => 0,
-	'version' => '0.4.0',
+	'version' => '1.0.0',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -34,14 +34,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.5.0-0.0.0',
+			'php' => '5.3.7-5.5.99',
+			'typo3' => '4.5.0-6.2.99',
 		),
-		'conflicts' => array(),
-		'suggests' => array(),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"d993";s:21:"ext_conf_template.txt";s:4:"a460";s:12:"ext_icon.gif";s:4:"3c44";s:17:"ext_localconf.php";s:4:"5e56";s:13:"locallang.xml";s:4:"f1fa";s:37:"hooks/class.tx_cpsstopdc_tslib_fe.php";s:4:"bdfb";}',
-	'suggests' => array(),
+	'_md5_values_when_last_written' => 'a:7:{s:9:"ChangeLog";s:4:"78d1";s:21:"ext_conf_template.txt";s:4:"a460";s:12:"ext_icon.gif";s:4:"3c44";s:17:"ext_localconf.php";s:4:"d6e8";s:13:"locallang.xml";s:4:"6902";s:52:"Tests/Unit/hooks/class.tx_cpsstopdc_tslib_feTest.php";s:4:"f8a1";s:37:"hooks/class.tx_cpsstopdc_tslib_fe.php";s:4:"330d";}',
+	'suggests' => array(
+	),
 );
 
 ?>
