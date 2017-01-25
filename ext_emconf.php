417ab5256fc65c5a514f3be0fe968ff0698589b7
Nicole Cordes <cordes@cps-it.de>
2017-01-25 16:24:27 +0100

    [TASK] Set dependency to TYPO3 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.7-5.5.99',
-			'typo3' => '4.5.0-6.2.99',
+			'php' => '5.3.7-7.1.99',
+			'typo3' => '6.2.0-8.5.99',
 		),
 		'conflicts' => array(
 		),
